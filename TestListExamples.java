7cd08523f41c59eda7198c1b9956a9392720b193
tron-e <93411591+tron-e@users.noreply.github.com>
2022-11-27 21:08:17 -0800

    j

--- TestListExamples.java
+++ TestListExamples.java
@@ -1,6 +1,41 @@
 import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.*;
+
+class LongString implements StringChecker {
+  public boolean checkString(String s) { return s.length() > 4; }
+}
+
 
 public class TestListExamples {
   // Write your grading tests here!
+  @Test
+public void testFilter(){
+  //Creates a list of Strings
+  List<String> aList = new ArrayList<String>();
+  //Creates an array of strings to be added to a list
+  String[] anArrayOfStrings = {"The", "He", "Hello", "Welcome"};
+  //adds our array to the list
+  aList.addAll(Arrays.asList(anArrayOfStrings));
+  List<String> result = ListExamples.filter(aList, new LongString());
+  String[] whatWeWant = {"Hello", "Welcome"};
+  assertArrayEquals(whatWeWant, result.toArray());
+}
+
+@Test
+public void testMerge(){
+  List<String> list1 = new ArrayList<String>();
+  list1.add("apple");
+  list1.add("orange");
+  list1.add("why");
+  List<String> list2 = new ArrayList<String>();
+  list2.add("ball");
+  list2.add("cat");
+  list2.add("zebra");
+  assertEquals(new ArrayList<String>(){{ add("apple"); add("ball"); add("cat"); add("orange"); add("why"); add("zebra"); }}, ListExamples.merge(list1, list2));
+}
 }
--- grade.sh
+++ grade.sh
@@ -1,7 +1,47 @@
 # Create your grading script here
 
-set -e
+CPATH=".:../lib/hamcrest-core-1.3.jar:../lib/junit-4.13.2.jar"
+file="TestListExamples.java"
 
-rm -rf student-submission
-git clone $1 student-submission
+rm -rf student-submission 
+git clone $1 student-submission 
+cp TestListExamples.java student-submission/
+cd student-submission 
 
+if [[ -f $file ]] && [[ -e $file ]]
+then
+    echo "File exists!"
+else
+    echo "File not found!"
+    echo "Grade 0/5"
+    exit 1
+fi
+
+javac -cp $CPATH *.java > return.txt 
+
+if [[ $? -eq 0 ]]
+then
+    echo "Compile succeeded"
+ 
+else
+    echo "Your program didn't compile"
+    echo "Grade 1/5"
+    exit 2
+   
+fi
+
+java -cp $CPATH  org.junit.runner.JUnitCore TestListExamples > results.txt 
+
+if [[ $? -eq 0 ]]
+then
+    echo "Test Passed"
+    echo "Grade 5/5"
+    cat results.txt
+    exit
+    
+else 
+    echo "Test Failed"
+    echo "Grade 3/5"
+    cat results.txt
+    exit
+fi
